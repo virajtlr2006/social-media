fecdb51984721bfe8090e245662a0a9c63bf1fa8
virajtlr2006 <virajtlr2006@gmail.com>
2026-01-02 09:59:52 +0530

    Feat(db/schema):setup done

---
+++ db/schema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const PostTable = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  username: varchar().notNull(),
+  title: varchar().notNull(),
+  description: varchar().notNull(),
+  image: varchar().notNull(),
+});
+
+export type Posts = typeof PostTable.$inferSelect;
